e22ce9e44eef14d5076799010a91dac870579430
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-05-02 14:56:33 +0200

    Symbol Newtona V8

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import math
 
-n = str(input())
-n = n.split(' ')
-numbers = [int(i) for i in n]
-x = numbers[0]
-y = numbers[1]
+x = str(input())
+x = x.split(' ')
+numbers = [int(i) for i in x]
+n = numbers[0]
+k = numbers[1]
 
 
 
-if y > x or len(numbers)>2 :
-    print(0)
-elif y == 1 or y == x: print(1)
-else:
-    a = math.factorial(x)
-    b = math.factorial(y)
-    div = a / (b*(x-y))
-    print(div)
+# if y > x or len(numbers)>2 :
+#     print(0)
+# elif y == 1 or y == x: print(1)
+# else:
+#     a = math.factorial(x)
+#     b = math.factorial(y)
+#     div = a // (b*(x-y))
+#     print(div)
 
+
+def Newton(n, k):
+    Wynik = 1
+    for i in range(1, k + 1):
+        Wynik = Wynik * (n - i + 1) / i
+    return Wynik
+
+print(Newton(n, k))
