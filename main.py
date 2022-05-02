041ff55a69ef23ef2d2ca80ee14d92c10e2a1375
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-05-02 14:14:37 +0200

    Symbol Newtona v1

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+n = str(input())
+n = n.split()
+numbers = [int(i) for i in n]
+x = numbers[0]
+y = numbers[1]
+
+if y == 1 or y == x:
+    print(1)
+
+if y > x:
+    print(0)
+else:
+    a = math.factorial(x)
+    b = math.factorial(y)
+    div = a // (b*(x-y))
+    print(div)
+
