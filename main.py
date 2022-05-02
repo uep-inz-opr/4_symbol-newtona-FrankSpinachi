1967525ecae7fe796374e7a92e13f02e26a2d886
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-05-02 14:19:36 +0200

    Symbol Newtona V2

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ numbers = [int(i) for i in n]
 x = numbers[0]
 y = numbers[1]
 
-if y == 1 or y == x:
-    print(1)
+
+
 
 if y > x:
     print(0)
+elif y == 1 or y == x: print(1)
 else:
     a = math.factorial(x)
     b = math.factorial(y)
